ed42034b799dff5d76cbc882ac283bdb9828f797
cortega <cortega@acciontip.cl>
2023-02-10 01:09:51 -0300

    chat

---
+++ backEnd/index.js
@@ -0,0 +1,62 @@
+const httpServer = require("http").createServer();
+
+let messages = [
+  {
+    colorCode: "aqua",
+    name: "cristobal",
+    message: "hola",
+  },
+];
+
+let countUser =0;
+
+let server = httpServer.listen(process.env.PORT || 3001, () => {
+  console.log("...");
+});
+let io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", function (socket) {
+  console.log("usuario conectado", socket.id);
+/*   countUser= countUser+1;
+  console.log(countUser);
+  socket.emit("countUser", countUser); */
+
+  socket.emit("messagesUsers", messages);
+
+  socket.on("sendMessages", (text) => {
+    messages.push({
+      name: text.name,
+      message: text.message,
+      colorCode: text.colorCode,
+    });
+    console.log(text);
+    io.emit("messagesUsers", messages);
+
+    if (messages.length > 200) {
+      messages = [
+        {
+          colorCode: 0,
+          name: "Admin",
+          message: "chat reset",
+        },
+      ];
+    }
+
+    /*   let busqueda = users.filter((list) => list.id == text.id);
+    if (busqueda == false) {
+      users.push({ id: text?.id });
+      console.log(users);
+      io.emit("listUsers", users);
+    } */
+  });
+
+  socket.on("disconnect", (text) => {
+    console.log("usuario desconectado");
+/*     countUser= countUser-1;
+    socket.emit("countUser", countUser); */
+  });
+});
---
+++ client/src/App.js
@@ -0,0 +1,174 @@
+import io from "socket.io-client";
+
+import { useEffect, useState, useRef } from "react";
+import "./App.css";
+import "./constant.css";
+
+let socket = io("http://localhost:3001/");
+function App() {
+  const containerRef = useRef(null);
+
+  const [messengersUsers, setMessengersUsers] = useState([{}]);
+
+  const [colorCode, setColorCode] = useState([
+    { codecolor: "#3a6c94" },
+    { codecolor: "brown" },
+    { codecolor: "coral" },
+    { codecolor: "blueviolet" },
+    { codecolor: "rgb(107, 107, 52)" },
+    { codecolor: "rgb(40, 129, 120)" },
+    { codecolor: "tomato" },
+    { codecolor: "teal" },
+    { codecolor: "slateblue" },
+    { codecolor: "sienna" },
+    { codecolor: "crimson" },
+    { codecolor: "darkgreen" },
+  ]);
+
+  const [stateComboBox, setStateComboBox] = useState(false);
+  const [stateColor, setStateColor] = useState({ stateColor: "#3a6c94" });
+  const [inputUser, setInputUser] = useState({
+    inputUserName: "",
+    inputMessenger: "",
+  });
+  const [stateChat, setStateChat] = useState(false);
+  const [stateCount, setStateCount] = useState(0);
+
+  const changeValueInputId = (e) => {
+    setInputUser({
+      ...inputUser,
+      [e.currentTarget.name]: e.currentTarget.value,
+    });
+  };
+
+  const ComboBoxColor = ({ hookColorCode, stateComboBox, hookStateColor }) => {
+    return (
+      <div className="divContainerComboBoxColor00 flexColumn">
+        <div
+          onClick={() => {
+            stateComboBox[1](true);
+          }}
+          style={{ backgroundColor: hookStateColor[0].stateColor }}
+          className="divContainerColorSelection00"
+        ></div>
+        <div
+          className="divContainerComboBoxColor01"
+          style={stateComboBox[0] ? { display: "block" } : { display: "none" }}
+        >
+          {hookColorCode.map((list) => {
+            return (
+              <div
+                onClick={() => {
+                  hookStateColor[1]({ stateColor: list.codecolor });
+                  stateComboBox[1](false);
+                }}
+              >
+                <div style={{ backgroundColor: list.codecolor }}></div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  };
+
+  useEffect(() => {
+    socket.on("messagesUsers", (messengersUsers) => {
+      setMessengersUsers(messengersUsers);
+    });
+  }, []);
+
+  useEffect(() => {
+    socket.on("countUser", (countUser) => {
+      setStateCount(countUser);
+    });
+  }, []);
+
+  useEffect(() => {
+    let totalHeightScroll =
+      containerRef.current.scrollHeight - containerRef.current.offsetHeight;
+    containerRef.current.scrollTop = totalHeightScroll;
+  }, [messengersUsers]);
+
+  return (
+    <div className="App">
+      <header>
+        <h1>Chat en proceso</h1>
+   {/*      <div>
+          <h4>Usuarios</h4> <h4>{stateCount}</h4>
+        </div> */}
+      </header>
+      <main>
+        <div className="divContainerChat00 flexColumn">
+          <div className="divContainerChat01">
+            <form
+              className="flexRow"
+              onSubmit={(e) => {
+                e.preventDefault();
+
+                /*      let stateName = messengersUsers.filter(
+                  (list) => list.name === inputUser.inputUserName
+                ); */
+              }}
+            >
+              <input
+                placeholder="Usuario"
+                name="inputUserName"
+                onChange={changeValueInputId}
+                value={inputUser.inputUserName}
+              ></input>
+              <ComboBoxColor
+                hookStateColor={[stateColor, setStateColor]}
+                hookColorCode={colorCode}
+                stateComboBox={[stateComboBox, setStateComboBox]}
+              />
+              {/*      <button name="enviar">Entrar</button> */}
+            </form>
+          </div>
+          <div ref={containerRef} className="divContainerChat02 flexColumn">
+            {messengersUsers.map((messengers) => {
+              return (
+                <div className="">
+                  <h4 style={{ color: messengers.colorCode }}>
+                    {messengers.name}
+                  </h4>
+                  <p>{messengers.message}</p>
+                </div>
+              );
+            })}
+          </div>
+          <div className="divContainerChat03">
+            <form
+              className="flexRow"
+              onSubmit={(e) => {
+                e.preventDefault();
+
+                if (inputUser.inputUserName.length > 3) {
+                  socket.emit("sendMessages", {
+                    colorCode: stateColor.stateColor,
+                    name: inputUser.inputUserName,
+                    message: inputUser.inputMessenger,
+                  });
+                } else {
+                  alert("usuario debe ser mayor a 3 letras");
+                }
+
+                setInputUser({ ...inputUser, inputMessenger: "" });
+              }}
+            >
+              <input
+                readOnly={stateChat}
+                name="inputMessenger"
+                onChange={changeValueInputId}
+                value={inputUser.inputMessenger}
+              ></input>
+              <button></button>
+            </form>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
